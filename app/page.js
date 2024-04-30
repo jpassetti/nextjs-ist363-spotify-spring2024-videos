e53cda9ce25f23422c1316c17b212a6ac09ef278
Jeff Passetti <jpassetti@gmail.com>
2024-04-30 18:40:27 -0400

    spotify api

--- app/page.js
+++ app/page.js
@@ -1,8 +1,37 @@
-const Homepage = () => {
+"use client";
+
+import { useEffect, useState } from "react";
+
+const HomePage = () => {
+ const [artists, setArtists] = useState([]);
+ const [isLoading, setIsLoading] = useState(true);
+ const [error, setError] = useState(null);
+
+ useEffect(() => {
+  try {
+   fetch("/api/artists")
+    .then((res) => res.json())
+    .then((data) => {
+     setArtists(data.artists);
+     setIsLoading(false);
+    });
+  } catch (error) {
+   setError(error);
+  }
+ }, []);
+
+ if (isLoading) return <p>Loading...</p>;
+ if (error) return <p>Error loading artists</p>;
+
+ console.log({ artists });
+
  return (
   <div>
-   <h1>Homepage</h1>
+   {artists.map((artist) => {
+    return <h1 key={artist.id}>{artist.name}</h1>;
+   })}
   </div>
  );
 };
-export default Homepage;
+
+export default HomePage;
